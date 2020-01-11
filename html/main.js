57f6a84abc10ab5c98cebf3c7345442a507b0682
Zandose <48503900+Zandose@users.noreply.github.com>
2020-01-11 18:38:55 -0500

    ds

--- html/main.js
+++ html/main.js
@@ -3,24 +3,3 @@ function myFunction1(x) {
     document.getElementById("myDropdown").classList.toggle("show");
 
 }
-
-/* When the user clicks on the button, toggle between hiding and showing the dropdown content */
-    function myFunction() {
-        document.getElementById("myDropdown").classList.toggle("show");
-    }
-
-    /*
-    // Close the dropdown menu if the user clicks outside of it
-    window.onclick = function(event) {
-        if (!event.target.matches('.dropbtn1')) {
-            var dropdowns = document.getElementsByClassName("container-content");
-            var i;
-            for (i = 0; i < dropdowns.length; i++) {
-                var openDropdown = dropdowns[i];
-                if (openDropdown.classList.contains('show')) {
-                    openDropdown.classList.remove('show');
-                }
-            }
-        }
-    }
-*/
